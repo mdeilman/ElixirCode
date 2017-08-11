201616190eff58cbc12614ce2c5bdbc8ee25e182
Mario Deilmann <mario.deilmann@googlemail.com>
2017-08-11 11:53:27 +0200

    Dummy push

---
+++ redfour/physics/test/physics_test.exs
@@ -0,0 +1,13 @@
+defmodule PhysicsTest do
+  use ExUnit.Case
+  doctest Physics
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+  
+  test "4 truth" do
+    assert 2 + 2 == 4
+  end
+  
+end
